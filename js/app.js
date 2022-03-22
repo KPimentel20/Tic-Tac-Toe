93c440c6cab17efa236af3eb84408a8d66adbe23
KP Pimentel <kppimentel@Autumns-MBP.lan>
2022-03-22 19:11:02 -0400

    here we are

--- js/app.js
+++ js/app.js
@@ -2,13 +2,10 @@
 
 let player1 = 'bunnyicon';
 let player2 = 'thirdeyeicon';
+let game = true;
+let draw = true || false;
 
-let tie = '';
-// let win = 'threeInARow' === 3;
-// let lose = noThree < MAX_TURNS;
-// let gameOver =
-
-const startGo = {
+const start = {
     '1': {
         name: 'player1',
         score: 0
@@ -34,19 +31,23 @@ const winnerPattern = [
 ];
 
 // setup event listeners//
+const begin = document.getElementById('start')
+let gameEl = document.getElementById("1").addEventListener('click', handleClick);
 let tableEl = document.querySelector('table').addEventListener('click', handleMove);
-let buttonEl = document.querySelector('button').addEventListener('click', initialize);
+// let buttonEl = document.querySelector('button').addEventListener('click',);
+
 
 /*----- functions -----*/
 // initialize to make the game your viewpoint at the beginning//
-function initliaze()
+// function initliaze()
 
 // whos turn is it is set initially to player 1//
 let whosTurnIsIt = player1
 
 //reason why theres no parameters is bc argument happens inside code block + there is no argument//
 //call/invoke alternateTurns when a player places an icon//
-function alternateTurns (){
+//wants me to add boolean to this//
+function alternateTurns(){
     if (whosTurnIsIt === player1) {
         whosTurnIsIt = player2
     } else if (whosTurnIsIt === player2) {
@@ -54,41 +55,47 @@ function alternateTurns (){
     }
     };
 
+function handleClick(e) {
+
+    console.log('handleClickInvoked')
+}
+
 function handleMove(e) {
     alternateTurns();
     console.log('handleMoveInvoked')
 }
 
-//who won//
-game.winnerPattern.forEach(() => {
-    let player1Won = winnerPattern.every(game.player1Pattern.includes(Pattern))
-    let player2Won = winnerPattern.every(game.player2Pattern.includes(Pattern))
+//its a tie//
 
-    if (player1Won || player2Won) {
-        document.querySelectorAll('box').forEach(cell => cell.id.add('disabled'))
-        document.querySelector('gameOver').id.add('visible')
-        document.querySelector('GAMEOVER').textContent = player1Won
-        ? 'player1Won!'
-        : 'player2Won!'
-    }
-})
-
-//restart the game//
-document.querySelector('REMATCH').addEventListener('click',() => {
-    document.querySelector('gameOver').id.remove('visible')
-    document.querySelectorAll('box').forEach(cell => {
-        cell.classList.remove('disabled', 'player1', 'player2')
-    })
-
-    game.player1Turn = true
-    game.player1Combo = []
-    game.player2Combo = []
-})
-
-function initialize(){
-    game = new Array(9).fill(null);
-    turns = 1;
-    winner = null;
-    // render();
+function tie(draw) {
+    if (draw) {
+        winnersMessageElement.innerText = "It's a tie!"
+    } else {
+        //${} is a string interpulation//
+        // winnersMessageElement.innerText = `player ${}` wins!
+    } winnersMessageElement.classList.add('show')
 }
 
+//who won//
+function checkWin(Winner) {
+if (rowTotal == 0 || columnTotal == 0 || rowTotal == 3 || columnTotal == 3) {
+    return true;
+} else if (rowTotal == 0 || columnTotal == 0 || rowTotal != 3 || columnTotal != 3) {
+    return false;
+}
+}
+
+const winnersMessageElement = document.getElementById('winnersMessage')
+
+//rematch/restart the game//
+const rematchButton = document.getElementById('PLAY AGAIN')
+
+// function gameOver() {
+// }
+
+// function initialize(){
+//     game = new Array(9).fill(null);
+//     turns = 1;
+//     winner = null;
+//     render();
+// }}
