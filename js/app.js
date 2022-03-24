bcae1cf5a4556207c147d0290af7b09e411b3be4
KP Pimentel <kppimentel@Autumns-MBP.lan>
2022-03-24 02:38:37 -0400

    not done yet

--- js/app.js
+++ js/app.js
@@ -1,29 +1,32 @@
 /*----- constants -----*/
-// The winner (3 ‘string’ options) => player1, player2, & tie//
+// The winner (3 ‘string’ options) => player1, player2, & draw//
 // Winner message definitely needs to pop up once: either when there is 3 in a row of one icon or when the board is full//
 
 // Required constraints:
 // Player one (objects with key value pairs: the keys (“icons”))//
 // Player two//
-//let is what can change + const is what can NOT change//
-let player1 = 'B';
-let player2 = 'T';
+let player1 = 'B'; //bunny icon//
+let player2 = 'T'; //third eye icon//
 let board; // array of 9 elements 
-let draw;
+let draw; //tie//
 let winner;
 let whosTurnIsIt;
 
+//player1 is blue//
+//player2 is red//
+//any box/space/square/cell that has yet to be clicked into will remain white(null)//
 const turns = {
     '1': 'blue',
     '-1': 'red',
     'null': 'white'
-    }
+}
 
 // Elements to access more than once in JS://
 // Cells of the grid of the gameboard//
 // Checking if there is a winner or a loser//
 // Player “names aka 1, & 2” for => who’s turn is it & for winner message//
 
+//each of these elements inside of our array will give us a winning pattern that the players need in order to win//
 const winnerPattern = [
     //rows//
     [0,1,2],
@@ -49,10 +52,11 @@ document.querySelector('section').addEventListener('click', handleMove);
 
 /*----- functions -----*/
 // initialize to make the game your viewpoint at the beginning//
-// whos turn is it is set initially to player 1//
+// whosTurnIsIt is set initially to player 1//
 
 init();
-
+//we are initializing our board, our variable(whosTurnIsIt aka player1 bc its 1) and our winner which for now is null until we have it rendered//
+//we have declared and initialized everything inside our array as null until players click into the game and begin//
 function init(){
     board = [null, null, null, null, null, null, null, null, null]
     whosTurnIsIt = 1;
@@ -60,9 +64,11 @@ function init(){
     render();
 }
 
-//we added an event and targeted where the event takes place (cell that was clicked on) to update the innerHTML//
-//move function for placing your constraints in cells/table//
-
+//handleMove is what allows our players to move//
+//indexof returns -1 which is in this game player1/whosTurnIsIt//
+//we are delaring that the index equals when we are targeting the event of each cell in the index of our index/array//
+//whosTurnIsIt has a value of 1 so if we multiply it by -1 it will allow us to alternate turns as it will result in -1/player2//
+//the same will continue bc we have rendered it and our win function comes after//
 function handleMove(e) {
     const idx = cellEls.indexOf(e.target);
     if (winner || board[idx]) return;
@@ -72,6 +78,9 @@ function handleMove(e) {
     render();
 }
 
+//The Math.abs lets us match a winning pattern in our array to the value of the numbers that a player has returned//
+//To win, player1 or player2 needs a winning pattern of 3 in a row/column/diaganol in our board/game//
+//If both player1 or player2 have the same winning pattern of 3 in a row/column/diaganol in our board/game it is then declared a draw//
 function checkWin(){
     for(let pattern of winnerPattern){
         if (Math.abs(board[pattern[0]] + board[pattern[1]] + board[pattern[2]]) === 3)
@@ -80,6 +89,8 @@ function checkWin(){
     return board.includes(null) ? null : 'draw';
     }
 
+//we are rendering our section(divs/cells)/the spaces in our grid to the index//
+//When we have a winner our rematch button will display, otherwise it will remain hidden//
 function render() {
     cellEls.forEach((el,idx) => {
     el.style.backgroundColor = turns[board[idx]];
